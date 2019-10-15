adb1e5b1f70a23be681f2940b766927f7aeb77c9
Yanggum <bak405@gmail.com>
2019-10-15 09:55:23 +0900

    Translator 구현

--- src/main/java/com/tia/main/App.java
+++ src/main/java/com/tia/main/App.java
@@ -3,6 +3,7 @@ package com.tia.main;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Hello world!
@@ -13,32 +14,42 @@ public class App
     public static void main( String[] args )
     {
     	// Input 정의
-    	String[][] test = new String[][] {{"1", "1", "2"}, {"2", "3", "5"}, {"3", "6", "9"} };    	
-    	
-    	int result = testRecusrsion(1);
-        System.out.println( "Hello World! = " + result);
-        
-    	while(true) {
-    		
-    		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    		
-    		String input = "";
+    	String[] test = new String[] {"가"};    	
+    	String test2 = new String("가힣");
+    	byte[] test3 = test2.getBytes(StandardCharsets.UTF_8);
+    	String test4 = "";
+		String result = ".,?!'\"";		
 
-    		try {
-				input = in.readLine();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-    		
-    		if (input.equals("q"))
-    			break;
-    		else {
-    			System.out.println(input);
-    		}
-    		
-    	}    	
-    }
+    	
+    	System.out.println(result.length());
+    	
+    	
+		/*
+		 * System.out.println("test3 =" + test3.toString()); System.out.println(a);
+		 * System.out.println(b);
+		 */    	
+    	
+//    	int result = testRecusrsion(1);
+//        System.out.println( "Hello World! = " + result);
+        
+        
+        
+        
+        
+		/*
+		 * while(true) {
+		 * 
+		 * BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		 * 
+		 * String input = "";
+		 * 
+		 * try { input = in.readLine(); } catch (IOException e) { // TODO Auto-generated
+		 * catch block e.printStackTrace(); }
+		 * 
+		 * if (input.equals("q")) break; else { System.out.println(input); }
+		 * 
+		 * }
+		 */    }
     
     public static int testRecusrsion(int i) {
     	
---
+++ src/main/java/com/tia/main/LoveTranslator.java
@@ -0,0 +1,35 @@
+package com.tia.main;
+
+public class LoveTranslator {
+	public static final int ALL_HANGUL_LENGTH = '힣'-'가';
+	public static final int ALL_MARKS = 6;
+	
+	
+	public static String getHangul(int i) {		
+		String result = "";
+		String hanguls = getHangulSet();
+		
+		result += hanguls.charAt(i);
+				
+		return result;
+	}
+	
+	
+	
+	
+	private static String getHangulSet() {
+		String result = "";    	
+    	
+    	for (int i='가';i<='힣' ;i++) {
+    		result += (char)i;
+    	}
+    	
+    	return result;
+	}
+	
+	public static String getMarks() {
+		String result = ".,?!'\"";		
+		return result;
+	}
+
+}
